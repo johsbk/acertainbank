5e63b0c665a4ffeab325e16a1d320bf664940b7a
= <johsbk@gmail.com>
2014-01-07 15:47:26 +0100

    test done

--- src/com/acertainbank/tests/CertainAccountManagerTest.java
+++ src/com/acertainbank/tests/CertainAccountManagerTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.acertainbank.business.CertainAccountManager;
+import com.acertainbank.exceptions.ExistentAccountException;
 import com.acertainbank.exceptions.InexistentAccountException;
 import com.acertainbank.exceptions.InexistentBranchException;
 import com.acertainbank.exceptions.NegativeAmountException;
@@ -23,12 +25,31 @@ public class CertainAccountManagerTest {
     @Test
     public void addAccounts() {
     	try {
+			accountManager.addAccount(2,1);
+			fail();
+		} catch (InexistentBranchException e1) {
+			assertTrue(true);
+		} catch (ExistentAccountException e1) {
+			fail();
+			e1.printStackTrace();
+		}
+    	try {
 	    	accountManager.addAccount(1,1,5.0);
 	    	assertEquals(5.0,accountManager.getAccountBalance(1,1),0.1);
     	} catch(Exception e) {
     		e.printStackTrace();
 	    	fail();
     	}
+    
+	    try {
+			accountManager.addAccount(1,1,5.0);
+			fail();
+		} catch (InexistentBranchException e) {
+			fail();
+			e.printStackTrace();
+		} catch (ExistentAccountException e) {
+			assertTrue(true);
+		}
     }
     
     @Test
@@ -41,7 +62,6 @@ public class CertainAccountManagerTest {
     	}
     	try {
 			accountManager.credit(1, 1, 5.0);
-			System.out.println(accountManager.getAccountBalance(1,1));
 			assertEquals(5.0,accountManager.getAccountBalance(1,1),0.1);
 		} catch (InexistentBranchException e) {
 			fail();
@@ -53,6 +73,71 @@ public class CertainAccountManagerTest {
 			fail();
 			e.printStackTrace();
 		}
+        try {
+			accountManager.credit(1, 1, -5.0);
+			fail();
+		} catch (InexistentBranchException e) {
+			fail();
+			e.printStackTrace();
+		} catch (InexistentAccountException e) {
+			fail();
+			e.printStackTrace();
+		} catch (NegativeAmountException e) {
+			assertTrue(true);
+		}
     	
+    }
+    
+    @Test
+    public void debitAccount() {
+    	try {
+    		accountManager.addAccount(1,1);
+    	} catch(Exception e) {
+    		e.printStackTrace();
+	    	fail();
+    	}
+    	try {
+			accountManager.debit(1, 1, 5.0);
+			assertEquals(-5.0,accountManager.getAccountBalance(1,1),0.1);
+		} catch (InexistentBranchException e) {
+			fail();
+			e.printStackTrace();			
+		} catch (InexistentAccountException e) {
+			fail();
+			e.printStackTrace();
+		} catch (NegativeAmountException e) {
+			fail();
+			e.printStackTrace();
+		}
+        try {
+			accountManager.debit(1, 1, -5.0);
+			fail();
+		} catch (InexistentBranchException e) {
+			fail();
+			e.printStackTrace();
+		} catch (InexistentAccountException e) {
+			fail();
+			e.printStackTrace();
+		} catch (NegativeAmountException e) {
+			assertTrue(true);
+		}
+    	
+    }
+    
+    @Test
+    public void calculateExposure() {
+    	try {
+    		accountManager.addAccount(1,1,-5.);
+    	} catch(Exception e) {
+    		e.printStackTrace();
+	    	fail();
+    	}
+    	try {
+			double exposure = accountManager.calculateExposure(1);
+			assertEquals(exposure,5.,0.1);
+		} catch (InexistentBranchException e) {
+			fail();
+			e.printStackTrace();
+		}
     }
 }
